cfb55532f15e800a313c97e5e7daddc67ef4bbfd
surajkumar22079 <90059548+surajkumar22079@users.noreply.github.com>
2023-05-20 23:13:48 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import Home_page from "./Components/Home_page";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Home_page/>
+      <Footer/>
     </div>
   );
 }
